e8c32775a3cafcf348aac2e9d080a25da74297f2
CoasterFreakDE <mleaked@gmail.com>
2024-08-13 16:07:59 +0200

    feat: Fixes

--- src/main/kotlin/one/devsky/PrivateChannels.kt
+++ src/main/kotlin/one/devsky/PrivateChannels.kt
@@ -2,14 +2,11 @@ package one.devsky
 
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.utils.MemberCachePolicy
 import net.dv8tion.jda.api.utils.cache.CacheFlag
 import one.devsky.extensions.getLogger
-import one.devsky.extensions.saveProperties
 import one.devsky.manager.Environment
 import one.devsky.manager.RegisterManager.registerAll
 import one.devsky.manager.RegisterManager.registerCommands
-import java.io.File
 import java.util.*
 
 class PrivateChannels {
@@ -24,15 +21,7 @@ class PrivateChannels {
     init {
         instance = this
 
-        val propertiesFile = "data.properties"
-        val file = File(propertiesFile)
-
-        if (!file.exists()) {
-            saveProperties()
-        }
-
         jda = JDABuilder.createDefault(Environment.getEnv("BOT_TOKEN"))
-            .setMemberCachePolicy(MemberCachePolicy.ALL)
             .disableCache(CacheFlag.SCHEDULED_EVENTS)
             .enableCache(CacheFlag.VOICE_STATE)
             .registerAll()
